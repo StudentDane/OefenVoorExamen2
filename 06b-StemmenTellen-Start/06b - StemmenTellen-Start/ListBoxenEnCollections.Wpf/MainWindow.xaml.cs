ed3a8d70db687459540980befe3254e7a61bd367
Danor <dane_gillis@hotmail.com>
2019-06-01 15:26:46 +0200

    initial commit
    test

---
+++ 06b-StemmenTellen-Start/06b - StemmenTellen-Start/ListBoxenEnCollections.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ListBoxenEnCollections.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] kandidaten;
+        int[] aantalStemmen;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+    }
+}
